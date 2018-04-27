d6924b6b4feeee3c87a11b00b55ea391a6b12005
WanNJ <omyjackwan@gmail.com>
2018-04-27 10:43:23 +0800

    Add LCD HelloWorld Demo

---
+++ Examples/LCDHelloWorld/LCDHelloWorld.ino
@@ -0,0 +1,20 @@
+#include "LiquidCrystal.h"
+
+// initialize the library by providing the nuber of pins to it
+LiquidCrystal lcd(8,9,4,5,6,7);
+
+void setup() {
+    lcd.begin(16,2);
+
+    // set cursor position to start of first line on the LCD
+    lcd.setCursor(0,0);
+    //text to print
+    lcd.print(" FYQ I LOVE YOU");
+    // set cusor position to start of next line
+    lcd.setCursor(0,1);
+    lcd.print("   FOREVER 221");
+}
+
+void loop(){
+    
+}
